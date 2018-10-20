eb007a840c63edf64666f23738fdce664f6c9f40
markodimitrije <markodimitrije@gmail.com>
2018-10-20 14:30:47 +0200

    Initial Commit

---
+++ tryWebApiAndSaveToRealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tryWebApiAndSaveToRealm
+//
+//  Created by Marko Dimitrijevic on 20/10/2018.
+//  Copyright © 2018 Marko Dimitrijevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
